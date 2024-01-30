0592f3ae6adaad8e66a3cf1fe6a9bbd9318afc09
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-01-30 20:32:11 +0600

    theme add

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Theme } from './Theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <ThemeProvider theme={Theme}>
+  <App />
+  <CssBaseline/>
+  </ThemeProvider>
+   
   </React.StrictMode>,
 )
